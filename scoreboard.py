c0c93f09ef29cc080925be6c7d766f0b9b602cf2
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-01-29 10:46:15 -0600

    Added final score to scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,3 @@
-from tkinter.constants import CENTER
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 20, "normal")
@@ -6,6 +5,8 @@ FONT = ("Arial", 20, "normal")
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.color("white")
         self.penup()
@@ -14,13 +15,22 @@ class ScoreBoard(Turtle):
         self.update_score_board()
 
     def update_score_board(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as updated_file:
+                updated_file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score_board()
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def update_score(self):
             self.score += 1
-            self.clear()
             self.update_score_board()
